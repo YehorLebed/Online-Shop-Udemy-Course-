d54f077cee65520283731b3442cb4b47ecf96659
Yehor Lebed <lebed.egor01@gmail.com>
2020-03-21 07:57:10 +0200

    rewrite mapStateToProps with reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+// Redux + Reselect
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import {createStructuredSelector} from 'reselect'
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
+//Components 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
@@ -14,8 +18,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 });
 
 const mapDispatchToProps = (dispatch) => ({
